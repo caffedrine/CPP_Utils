404bbe1c0ab5c1975521f286982f0303c66b6732
caffedrine <bacchusstyle@yahoo.com>
2018-09-25 16:50:46 +0300

    time utils

--- time_utils.h
+++ time_utils.h
@@ -3,20 +3,17 @@
 /* Get time stamp in milliseconds */
 static inline uint64_t millis()
 {
-	return (uint64_t)std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::
-																		now().time_since_epoch()).count();
+	return (uint64_t)std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
 }
 
 /* Get time stamp in microseconds */
 static inline uint64_t micros()
 {
-	return (uint64_t)std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::
-																		now().time_since_epoch()).count();
+	return (uint64_t)std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
 }
 
 /* Get time stamp in nanoseconds */
 static inline uint64_t nanos()
 {
-	return (uint64_t)std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::
-																	   now().time_since_epoch()).count();
+	return (uint64_t)std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
 }
