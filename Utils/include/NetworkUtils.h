3f26dc836ed3bbf17d939c54f5a42ccc939e972a
caffedrine <bacchusstyle@yahoo.com>
2019-06-23 01:19:36 +0300

    Updated structure and optimized functions

--- Utils/include/NetworkUtils.h
+++ Utils/include/NetworkUtils.h
@@ -5,8 +5,20 @@
 #include <vector>
 #include <StringUtils.h>
 
-#include <netdb.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <netinet/ip_icmp.h>
+#include <time.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <time.h>
 
 namespace NetworkUtils
 {
@@ -16,7 +28,7 @@ namespace NetworkUtils
         {
             std::string result = *mainStr;
             size_t pos = std::string::npos;
-        
+            
             // Search for the substring in string in a loop untill nothing is found
             while( (pos = result.find(*toErase)) != std::string::npos )
             {
@@ -25,11 +37,15 @@ namespace NetworkUtils
             }
             return result;
         }
-    }
-    
-    char const * Ip2Hostname(const char *ip)
-    {
-        return "not_implemented";
+        
+        bool IsValidIPv4(const char *IPv4)
+        {
+            struct sockaddr_in sa;
+            int result = inet_pton(AF_INET, IPv4, &(sa.sin_addr));
+            if(result != 0)
+                return true;
+            return false;
+        }
     }
     
     bool IsValidHostnameFormat(const char *hostname)
@@ -41,30 +57,47 @@ namespace NetworkUtils
     {
         struct addrinfo hints = {}, *res;
         
-        memset (&hints, 0, sizeof (hints));
+        memset(&hints, 0, sizeof(hints));
         hints.ai_family = PF_UNSPEC;
         hints.ai_socktype = SOCK_STREAM;
         hints.ai_flags |= AI_CANONNAME;
-    
-        if (getaddrinfo (hostname, NULL, &hints, &res) != 0)
+        
+        if( getaddrinfo(hostname, NULL, &hints, &res) != 0 )
         {
             return false;
         }
         return true;
     }
     
-    char const * Hostname2Ip(const char *hostname)
+    char const *Ip2Hostname(const char *ip)
+    {
+        struct hostent *hent;
+        struct in_addr addr = {};
+        char result[255];
+        
+        if( !inet_aton(ip, &addr) )
+        {
+            return (ip);
+        }
+        if( (hent = gethostbyaddr((char *) &(addr.s_addr), sizeof(addr.s_addr), AF_INET)) )
+        {
+            return hent->h_name;
+        }
+        return "";
+    }
+    
+    char const *Hostname2Ip(const char *hostname)
     {
         /* If it is already a valid IP just return it */
         struct sockaddr_in sa = {};
         int isIp = inet_pton(AF_INET, hostname, &(sa.sin_addr));
-        if(isIp != 0)
+        if( isIp != 0 )
             return hostname;
         
         /* Return if hostname is invalid */
-        if(!IsValidHostnameFormat(hostname))
+        if( !IsValidHostnameFormat(hostname) )
             return "invalid_hostname";
-    
+        
         struct addrinfo hints = {}, *res;
         char addrstr[100];
         void *ptr;
@@ -72,22 +105,22 @@ namespace NetworkUtils
         char result[116]; // max 1 IPv4 and 1IPv6 and ','
         memset(result, '\0', sizeof(result));
         
-        memset (&hints, 0, sizeof (hints));
+        memset(&hints, 0, sizeof(hints));
         hints.ai_family = PF_UNSPEC;
         hints.ai_socktype = SOCK_STREAM;
         hints.ai_flags |= AI_CANONNAME;
         
-        if (getaddrinfo (hostname, NULL, &hints, &res) != 0)
+        if( getaddrinfo(hostname, NULL, &hints, &res) != 0 )
         {
             return "";
         }
         
         int ResultsNo = 0;
-        while (res)
+        while( res )
         {
-            inet_ntop (res->ai_family, res->ai_addr->sa_data, addrstr, 100);
-        
-            switch (res->ai_family)
+            inet_ntop(res->ai_family, res->ai_addr->sa_data, addrstr, 100);
+            
+            switch( res->ai_family )
             {
                 case AF_INET:
                     ptr = &((struct sockaddr_in *) res->ai_addr)->sin_addr;
@@ -96,14 +129,14 @@ namespace NetworkUtils
                     ptr = &((struct sockaddr_in6 *) res->ai_addr)->sin6_addr;
                     break;
             }
-            inet_ntop (res->ai_family, ptr, addrstr, 100);
+            inet_ntop(res->ai_family, ptr, addrstr, 100);
             //printf ("IPv%d address: %s (%s)\n", res->ai_family == PF_INET6 ? 6 : 4, addrstr, res->ai_canonname);
-            if(ResultsNo > 0)
+            if( ResultsNo > 0 )
             {
                 strcat(result, ",");
             }
             
-            if(res->ai_family == PF_INET6)
+            if( res->ai_family == PF_INET6 )
             {
                 strcat(result, addrstr);
             }
@@ -115,7 +148,6 @@ namespace NetworkUtils
             ResultsNo++;
         }
         
-        
         char *ret = result;
         return ret;
     }
@@ -124,12 +156,13 @@ namespace NetworkUtils
     {
         std::string urlcopy = *url;
         
-        urlcopy = _Utils::StringEraseAllSubStr (&urlcopy, (std::string *)"http://");
-        urlcopy = _Utils::StringEraseAllSubStr (&urlcopy, (std::string *)"https://");
+        urlcopy = _Utils::StringEraseAllSubStr(&urlcopy, (std::string *) "http://");
+        urlcopy = _Utils::StringEraseAllSubStr(&urlcopy, (std::string *) "https://");
         urlcopy = urlcopy.substr(0, urlcopy.find("/"));
         
         return urlcopy;
     }
+    
 }
 
 #endif
--- Utils/src/main.cpp
+++ Utils/src/main.cpp
@@ -3,6 +3,7 @@
 #include "Utils/Logger.h"
 #include "NetworkUtils.h"
 #include "IpUtils.h"
+#include "Ping.h"
 
 using std::vector;
 using std::string;
@@ -13,17 +14,21 @@ int main()
 {
 	setup_logger();
     vector<string> IPs;
+    IPs.emplace_back("1.2.3.4");
     IPs.emplace_back("62.4.15.44");
     IPs.emplace_back("direct.1way.pro");
     IPs.emplace_back("8.8.8.8");
     IPs.emplace_back("www.google.com");
     IPs.emplace_back("random_wrong_str");
-    
+    IPs.emplace_back("192.168.1.1");
+    IPs.emplace_back("123.123.123.123");
     
     for( string &ip: IPs )
     {
-        log("'{}' is valid ip       : {}", ip, IpUtils::IsValidIPv4(ip.c_str()) );
-        log("'{}' is valid hostname : {}", ip, NetworkUtils::IsValidHostname(ip.c_str()) );
-        log("'{}' host2ip           : {}\n", ip, string(NetworkUtils::Hostname2Ip(ip.c_str())) );
+//        log("'{}' is valid ip       : {}", ip, IpUtils::IsValidIPv4(ip.c_str()) );
+//        log("'{}' is valid hostname : {}", ip, NetworkUtils::IsValidHostname(ip.c_str()) );
+//        log("'{}' host2ip           : {}", ip, string(NetworkUtils::Hostname2Ip(ip.c_str())) );
+//        log("'{}' ip2host           : {}", ip, string(NetworkUtils::Ip2Hostname(ip.c_str())) );
+        log("'{}' ping attempt      : {}", ip, NetworkUtils::Ping(ip.c_str(), 1000));
     }
 }
